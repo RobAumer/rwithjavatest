9f1c78f5f151e38779948b807db490cca2150b0a
RobAumer <raumer@medianewsgroup.com>
2021-10-11 14:41:09 -0600

    makes a change to the jar file

--- java/HelloJavaWorld.java
+++ java/HelloJavaWorld.java
@@ -1,6 +1,6 @@
 public class HelloJavaWorld {
 public String sayHello() {
-String result = new String("Hello Java World!");
+String result = new String("Hello Java World 2!");
 return result;
 }
 public static void main(String[] args) {

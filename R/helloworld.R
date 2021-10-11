c5c3b51d60d789b6949727cf695cb709535f4e11
RobAumer <raumer@medianewsgroup.com>
2021-10-11 14:33:58 -0600

    adds broken jar file

---
+++ R/helloworld.R
@@ -0,0 +1,11 @@
+#' Title
+#'
+#' @return out a message
+#' @export
+#'
+#' @examples
+helloJavaWorld <- function(){
+  hjw <- .jnew("HelloJavaWorld") # create instance of HelloJavaWorld class
+  out <- .jcall(hjw, "S", "sayHello") # invoke sayHello method
+  return(out)
+}
